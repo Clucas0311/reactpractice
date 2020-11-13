f5af7db4dc87eda16eac0061b32df1e27e1059fc
Clucas0311 <clucas0311@gmail.com>
2020-11-12 19:48:06 -0500

    Creating variables

--- src/index.js
+++ src/index.js
@@ -2,18 +2,20 @@
 // import the variable name from a library
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+function getButtonText() {
+	return 'Click on me!';
+}
 // Create a react component
 // a function or a class
 // area for event handler, showing content to the user
 const App = function() {
 	return (
 		<div>
-			<label class="label" for="name">
+			<label className="label" for="name">
 				Enter name:
 			</label>
 			<input id="name" type="text" />
-			<button style={{ backgroundColor: 'blue', color: 'white' }}>Submit</button>
+			<button style={{ backgroundColor: 'blue', color: 'white' }}>{getButtonText()}</button>
 		</div>
 	);
 };
